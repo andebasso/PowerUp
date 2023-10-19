7c2fa612978b51a3ce5478433125f8f9a85bbdbb
Anderson <andebasso@yahoo.com.br>
2023-10-19 17:22:09 -0300

    mudanças em todos arquivos

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const t = TrelloPowerUp.iframe();
+var t = TrelloPowerUp.iframe({ secret: '33029194858928579285918998792' }); // Substitua 'your-secret-key' por uma chave secreta de sua escolha
 
-t.render(function() {
-    // Nothing to render in this basic example.
-});
-
-function showHelloWorld(t, options) {
-    // This function will add a "Hello World" comment to the card's activity feed.
-    return t.card('id').then(function(card) {
-        return t.post(`/cards/${card.id}/actions/comments`, { text: 'Hello World' });
-    });
-}
-
-// Register our callback function
-TrelloPowerUp.initialize({
-    'card-buttons': function(t, options) {
-        return [{
-            icon: 'https://andebasso.github.io/PowerUp/icon.png',
-            text: 'Say Hello World',
-            callback: showHelloWorld
-        }];
-    }
-});
+// Adicione mais lógicas de Power-Up conforme necessário
