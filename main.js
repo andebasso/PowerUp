04d842284e301a984287726a8ba190e412dbe3d6
Anderson <andebasso@yahoo.com.br>
2023-10-19 17:26:06 -0300

    mudança para mais simples

--- main.js
+++
@@ -1,3 +0,0 @@
-var t = TrelloPowerUp.iframe({ secret: '33029194858928579285918998792' }); // Substitua 'your-secret-key' por uma chave secreta de sua escolha
-
-// Adicione mais lógicas de Power-Up conforme necessário
