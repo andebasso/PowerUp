2cbcaa332b3629fb0fcf1fd71c06ebbe9c2cb137
Anderson <andebasso@yahoo.com.br>
2023-10-19 14:43:23 -0300

    corrige código js para o Power-Up

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 var t = TrelloPowerUp.iframe();
 
+window.openKPI = function(t, options) {
+  return t.modal({
+    url: t.signUrl('https://andebasso.github.io/PowerUp/index.html'),
+    accentColor: '#0079BF',
+    fullscreen: true,
+    title: 'Trello KPI'
+  });
+}
+
 document.getElementById('saveData').addEventListener('click', function() {
     const activityName = document.getElementById('activityName').value;
     const startDate = document.getElementById('startDate').value;
     const endDate = document.getElementById('endDate').value;
+
     t.set('card', 'shared', 'activityData', {
         name: activityName,
         startDate: startDate,
@@ -20,8 +30,8 @@ document.getElementById('generateReport').addEventListener('click', function() {
         cards.forEach(function(card) {
             t.get(card.id, 'shared', 'activityData').then(function(activityData) {
                 if (activityData && activityData.startDate && activityData.endDate) {
-                    let start = new Date(activityData.startDate);
-                    let end = new Date(activityData.endDate);
+                    let start = moment(activityData.startDate, "YYYY-MM-DD");
+                    let end = moment(activityData.endDate, "YYYY-MM-DD");
                     let duration = (end - start) / (1000 * 60 * 60 * 24); // duration in days
                     totalDuration += duration;
                     count++;
